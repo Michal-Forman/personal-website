325d3b2fef5a7ba9e42cc795b3c4cc999196de57
michalforman <miska.forman@gmail.com>
2023-12-13 10:07:21 +0100

    add language buttons functionality and prepare for adding trasnlation feature

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ for (let i = 0; i < otherPageLinks.length; i++) {
     alert("This page is still in development. Please come back later.");
   });
 }
-// Add event listeners to language buttons, while translation is not yet available
-languageButtons = document.getElementsByClassName("language_button");
-for (let i = 0; i < languageButtons.length; i++) {
-  languageButtons[i].addEventListener("click", function (event) {
-    event.preventDefault();
-    alert("Language translation still in development.");
-  });
+// Add translation feature
+
+let languageButton1 = document.getElementById("language1");
+let languageButton2 = document.getElementById("language2");
+let dropdownBtnTitle = document.getElementById("dropdown-btn_title");
+
+let languages = ["en", "cz", "de"];
+let activeLanguage = languages[0];
+
+function language1_clicked() {
+  languageButton1.innerHTML = activeLanguage.toUpperCase();
+  dropdownBtnTitle.innerHTML = languages[1].toUpperCase();
+  [languages[0], languages[1]] = [languages[1], languages[0]];
+  activeLanguage = languages[0];
+  translate();
+}
+
+function language2_clicked() {
+  languageButton2.innerHTML = activeLanguage.toUpperCase();
+  dropdownBtnTitle.innerHTML = languages[2].toUpperCase();
+  [languages[0], languages[2]] = [languages[2], languages[0]];
+  activeLanguage = languages[0];
+  translate();
+}
+
+function translate() {
+  console.log(activeLanguage);
 }
